88035fbd469a83aca9091dc7391eab09dca4d900
Alextxnk <alexsolov766@gmail.com>
2022-10-27 23:53:33 +0300

    change component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -22,12 +22,17 @@ const Counter = () => {
    const handleTagChange = (id) => {
       setTags((prevState) => prevState.filter(tag => tag !== id));
    };
+   const renderTags = () => {
+      return (
+         tags.length !== 0 ? tags.map((tag) => (
+            <li key={tag} className='btn btn-primary btn-sm m-2' onClick={() => handleTagChange(tag)}>{tag}</li>
+         )) : 'No tags'
+      );
+   };
 
    return (
       <>
-         <ul>{tags.map((tag) => (
-            <li key={tag} className='btn btn-primary btn-sm m-2' onClick={() => handleTagChange(tag)}>{tag}</li>
-         ))}</ul>
+         <ul>{renderTags()}</ul>
          <span className={getBageClasses()}>{formatCount()}</span>
          <button className='btn btn-primary btn-sm m-2' onClick={handleIncrement}>+</button>
          <button className='btn btn-primary btn-sm m-2' onClick={handleDecrement}>-</button>
