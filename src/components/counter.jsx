b54c218226385996285cfb2fbec622417ce3aa05
Alextxnk <alexsolov766@gmail.com>
2022-10-27 22:25:11 +0300

    first commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Counter = () => {
+   const count = 2;
+   
+   return (
+      <>
+         <h1>{count}</h1>
+         <button>+</button>
+      </>
+   );
+};
+
+export default Counter;
