2214044511425a6f3bc5715171457f29483b433f
Alextxnk <alexsolov766@gmail.com>
2022-10-28 15:18:53 +0300

    change component

--- src/index.js
+++ src/index.js
@@ -5,4 +5,8 @@ import App from './app';
 
 const container = document.getElementById('app');
 const root = createRoot(container); 
-root.render(<App />);
+root.render(
+   <React.StrictMode>
+      <App />
+   </React.StrictMode>
+);
