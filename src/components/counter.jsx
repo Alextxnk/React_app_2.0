497e4938cd920e7d50542b9ea586c530f1bf9c78
Alextxnk <alexsolov766@gmail.com>
2022-10-28 15:50:54 +0300

    change component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -27,6 +27,7 @@ const Counter = (props) => {
          <span className={getBageClasses()}>{formatValue()}</span>
          <button className='btn btn-primary btn-sm m-2' onClick={handleIncrement}>+</button>
          <button className='btn btn-primary btn-sm m-2' onClick={handleDecrement}>-</button>
+         <button className='btn btn-danger btn-sm m-2' onClick={props.onDelete}>Delete</button>
       </div>
    );
 };
