98dc23f05bd09c2c48a179dff5e011c48926adb7
Alextxnk <alexsolov766@gmail.com>
2022-10-28 15:09:39 +0300

    change component

--- src/components/countersList.jsx
+++ src/components/countersList.jsx
@@ -2,11 +2,19 @@ import React from 'react';
 import Counter from './counter';
 
 const CountersList = () => {
-   const counters = [{id:0, value:0}, {id:1, value:4}, {id:2, value:3}];
+   const counters = [
+      { id: 0, value: 0, name: 'Ненужная вещь' }, 
+      { id: 1, value: 4, name: 'Ложка' }, 
+      { id: 2, value: 2, name: 'Вилка' }, 
+      { id: 3, value: 1, name: 'Тарелка' }, 
+      { id: 4, value: 3, name: 'Набор минималиста' }
+   ];
 
    return (
       <>
-         {counters.map(count => <Counter key={count.id} value={count.value} />)}
+         {counters.map((count) => (
+            <Counter key={count.id} value={count.value} name={count.name} /> 
+         ))}
       </>
    );
 };
