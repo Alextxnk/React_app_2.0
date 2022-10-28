5349f7ab4943d40bdc27e6537423393bf2736359
Alextxnk <alexsolov766@gmail.com>
2022-10-28 18:29:03 +0300

    change component

--- src/components/countersList.jsx
+++ src/components/countersList.jsx
@@ -21,12 +21,25 @@ const CountersList = () => {
       setCounters(initialState);
    };
 
+   const handleUpdate = () => {
+      const updatedState = [
+         { id: 0, value: 5, name: 'Ненужная вещь' },
+         { id: 1, value: 1, name: 'Ложка' },
+         { id: 2, value: 7, name: 'Вилка' },
+         { id: 3, value: 3, name: 'Тарелка' },
+         { id: 4, value: 6, name: 'Набор минималиста' }
+      ];
+
+      setCounters(updatedState);
+   };
+
    return (
       <>
          {counters.map((count) => (
             <Counter key={count.id} onDelete={handleDelete} {...count} />
          ))}
          <button className='btn btn-primary btn-sm m-2' onClick={handleReset}>Сброс</button>
+         <button className='btn btn-primary btn-sm m-2' onClick={handleUpdate}>Обновить состояние</button>
       </>
    );
 };
