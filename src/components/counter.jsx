5f6db5d8483d088508193f8125d93f5049d6b0a4
Alextxnk <alexsolov766@gmail.com>
2022-10-28 12:53:26 +0300

    change component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,6 @@ import React, { useState } from 'react';
 
 const Counter = () => {
    const [count, setCount] = useState(0);
-   const [tags, setTags] = useState(['tag1', 'tag2', 'tag3']);
 
    const formatCount = () => {
       return count === 0 ? 'empty' : count;
@@ -23,25 +22,12 @@ const Counter = () => {
       setCount((prevState) => prevState - 1);
    };
 
-   const handleTagChange = (id) => {
-      setTags((prevState) => prevState.filter(tag => tag !== id));
-   };
-
-   const renderTags = () => {
-      return (
-         tags.length !== 0 ? tags.map((tag) => (
-            <li key={tag} className='btn btn-primary btn-sm m-2' onClick={() => handleTagChange(tag)}>{tag}</li>
-         )) : 'No tags'
-      );
-   };
-
    return (
-      <>
-         <ul>{renderTags()}</ul>
+      <div>
          <span className={getBageClasses()}>{formatCount()}</span>
          <button className='btn btn-primary btn-sm m-2' onClick={handleIncrement}>+</button>
          <button className='btn btn-primary btn-sm m-2' onClick={handleDecrement}>-</button>
-      </>
+      </div>
    );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counter from './components/counter';
+import App from './app';
 
-
-const App = () => {
-   return (<Counter />);
-};
 
 const container = document.getElementById('app');
 const root = createRoot(container); 
